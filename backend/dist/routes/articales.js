5a5d2cafe658c303530b2f1290de691d454cefba
Kabir-Iqbal <kabirhussain0343837@gmail.com>
2025-09-20 15:18:38 +0500

    updated code and dependencies

--- backend/dist/routes/articales.js
+++ backend/dist/routes/articales.js
@@ -1,13 +1,4 @@
 "use strict";
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
@@ -18,10 +9,10 @@ const auth_1 = __importDefault(require("../middleware/auth")); // ✅ import Aut
 // This is a route object in which we define all the routes related to articles
 const router = express_1.default.Router();
 // Get all published articles
-router.get('/', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+router.get('/', async (req, res) => {
     try {
         // Check if database have any published articles
-        const articles = yield Artical_1.default.find({ status: 'published' }).populate('author', 'username');
+        const articles = await Artical_1.default.find({ status: 'published' }).populate('author', 'username');
         // then we send articals to the client in json format
         res.json(articles);
     }
@@ -29,12 +20,12 @@ router.get('/', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
         // if there is any error then we send the error message to the client
         res.status(500).json({ error: err.message });
     }
-}));
+});
 // Get a single article by id
-router.get('/:id', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+router.get('/:id', async (req, res) => {
     try {
         // Check if article is present in the database
-        const article = yield Artical_1.default.findById(req.params.id).populate('author', 'username');
+        const article = await Artical_1.default.findById(req.params.id).populate('author', 'username');
         // if article is not present then we send 404 error
         if (!article || article.status !== 'published')
             return res.status(404).json({ msg: 'Article not found' });
@@ -45,9 +36,9 @@ router.get('/:id', (req, res) => __awaiter(void 0, void 0, void 0, function* () 
     catch (err) {
         res.status(500).json({ error: err.message });
     }
-}));
+});
 // Create a new article and auth is required
-router.post('/', auth_1.default, (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+router.post('/', auth_1.default, async (req, res) => {
     // get the data from the request body
     const { title, category, body, status } = req.body;
     // check if the data is present in the request body
@@ -63,7 +54,7 @@ router.post('/', auth_1.default, (req, res) => __awaiter(void 0, void 0, void 0,
             author: req.user.id, status
         });
         // save the article to the database
-        yield article.save();
+        await article.save();
         // send the article to the client
         res.status(201).json(article);
     }
@@ -71,9 +62,9 @@ router.post('/', auth_1.default, (req, res) => __awaiter(void 0, void 0, void 0,
         // if there is any error then we send the error message to the client
         res.status(400).json({ error: err.message });
     }
-}));
+});
 // Update an article and auth is required
-router.put('/:id', auth_1.default, (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+router.put('/:id', auth_1.default, async (req, res) => {
     // get the data from the request body
     const { title, category, body, status } = req.body;
     // check if the data is present in the request body
@@ -82,7 +73,7 @@ router.put('/:id', auth_1.default, (req, res) => __awaiter(void 0, void 0, void 
     }
     try {
         // Check if article is present in the database
-        let article = yield Artical_1.default.findById(req.params.id);
+        let article = await Artical_1.default.findById(req.params.id);
         // if article is not present in database then we send 404 error
         if (!article)
             return res.status(404).json({ msg: 'Article not found' });
@@ -90,7 +81,7 @@ router.put('/:id', auth_1.default, (req, res) => __awaiter(void 0, void 0, void 
         if (article.author.toString() !== req.user.id)
             return res.status(403).json({ msg: 'Not authorized' });
         // if user is the author of the article then we update the article
-        article = yield Artical_1.default.findByIdAndUpdate(req.params.id, {
+        article = await Artical_1.default.findByIdAndUpdate(req.params.id, {
             title,
             category,
             body,
@@ -102,12 +93,12 @@ router.put('/:id', auth_1.default, (req, res) => __awaiter(void 0, void 0, void 
     catch (err) {
         res.status(400).json({ error: err.message });
     }
-}));
+});
 // Delete an article and auth is required
-router.delete('/:id', auth_1.default, (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+router.delete('/:id', auth_1.default, async (req, res) => {
     try {
         // Check if article is present in the database
-        const article = yield Artical_1.default.findById(req.params.id);
+        const article = await Artical_1.default.findById(req.params.id);
         // if article is not present in database then we send 404 error
         if (!article)
             return res.status(404).json({ msg: 'Article not found' });
@@ -115,13 +106,13 @@ router.delete('/:id', auth_1.default, (req, res) => __awaiter(void 0, void 0, vo
         if (article.author.toString() !== req.user.id)
             return res.status(403).json({ msg: 'Not authorized' });
         // if user is the author of the article then we delete the article
-        yield Artical_1.default.findByIdAndDelete(req.params.id);
+        await Artical_1.default.findByIdAndDelete(req.params.id);
         res.json({ msg: 'Article deleted' });
     }
     catch (err) {
         res.status(400).json({ error: err.message });
     }
-}));
+});
 exports.default = router;
 // import { Router, Request, Response } from 'express';
 // import { MongoClient } from 'mongodb';
--- backend/dist/utils/db.js
+++ backend/dist/utils/db.js
@@ -1,13 +1,4 @@
 "use strict";
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
@@ -16,7 +7,7 @@ const mongoose_1 = __importDefault(require("mongoose"));
 const dotenv_1 = __importDefault(require("dotenv"));
 // Load environment variables from .env file
 dotenv_1.default.config();
-const connectDB = () => __awaiter(void 0, void 0, void 0, function* () {
+const connectDB = async () => {
     if (mongoose_1.default.connection.readyState >= 1) {
         return;
     }
@@ -25,11 +16,11 @@ const connectDB = () => __awaiter(void 0, void 0, void 0, function* () {
         throw new Error("MONGO_URI environment variable is not defined");
     }
     try {
-        yield mongoose_1.default.connect(mongoUri);
+        await mongoose_1.default.connect(mongoUri);
         console.log("✅ Connected to MongoDB");
     }
     catch (error) {
         console.log("❌ MongoDB connection error:", error);
     }
-});
+};
 exports.default = connectDB;
